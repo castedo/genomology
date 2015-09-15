eeba1574a62c152d147e39e8cb14e35ddb88ebef
Castedo Ellerman <castedo@castedo.com>
2015-09-15 13:52:00 +0000

    filter out bad AncestryDNA raw data

--- package/R/genotype.R
+++ package/R/genotype.R
@@ -4,11 +4,12 @@ genotype.levels <- c('CC','GG','TT','AA','GC','TG','AT','TC','AG','AC')
 as.genotype <- function(allele1, allele2) {
   stopifnot(identical(levels(allele1), nucleotide.levels))
   stopifnot(identical(levels(allele2), nucleotide.levels))
-  n1 <- as.integer(allele1) - 1
-  n2 <- as.integer(allele2) - 1
-  d <- abs(n1 - n2)
-  nn <- pmin(n1, n2) + (d * (9 - d)) %/% 2
-  factor(nn, 0:9, genotype.levels)
+  i1 <- as.integer(allele1)
+  i2 <- as.integer(allele2)
+  ## this is just a convenient arithemetic mapping to the range 1 to 10
+  d <- abs(i1 - i2)
+  nn <- pmin(i1, i2) + (d * (9 - d)) %/% 2
+  factor(nn, 1:10, genotype.levels)
 }
 
 homozygous <- function(genotype) { as.integer(genotype) <= 4 }
@@ -33,12 +34,21 @@ nucleotide.in.genotype <- function(nucleotide, genotype) {
   return(as.logical(bitwAnd(n, mask[as.integer(genotype)])))
 }
 
-read.ancestrydna <- function(file) {
+read.rawancestrydna <- function(file) {
   cols <- c('character', 'integer', 'integer', 'character', 'character')
   df <- read.table(file, header=TRUE, colClasses=cols, as.is=TRUE)
   df$allele1 <- factor(df$allele1, nucleotide.levels)
   df$allele2 <- factor(df$allele2, nucleotide.levels)
+  return(df)
+}
+
+read.ancestrydna <- function(file) {
+  df <- read.rawancestrydna(file)
   df$genotype <- as.genotype(df$allele1, df$allele2)
+  # remove rare highly suspect data (often bogus)
+  bad <- (as.integer(df$allele1) < as.integer(df$allele2) |
+          df$genotype %in% c('GC', 'AT'))
+  df$genotype[bad] <- NA
   return(df)
 }
 
