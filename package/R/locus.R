b7f6d513062a2486959f629724159fbb7404a626
Castedo Ellerman <castedo@castedo.com>
2015-09-15 13:08:16 +0000

    add genous locus (gene location) related functions

---
+++ package/R/locus.R
@@ -0,0 +1,37 @@
+
+dataEnv <- new.env()
+
+set.genomology.loci <- function(df) {
+  stopifnot(is(df, "data.frame"))
+  stopifnot(all(c("locus", "chromosome", "start", "stop") %in% names(df)))
+  stopifnot(all(is.numeric(df$start)))
+  stopifnot(all(is.numeric(df$stop)))
+  assign("loci", df, envir=dataEnv)
+}
+
+genoposition <- function(chromosome, position) { chromosome * 2^28 + position }
+
+locus.at <- function(chromosome, position) {
+  stopifnot(length(chromosome) == length(position))
+  stopifnot(is.numeric(chromosome))
+  stopifnot(is.numeric(position))
+  if (!exists("loci", envir=dataEnv)) stop("must call set.genomology.loci")
+  loci <- get("loci", envir=dataEnv)
+  goff <- genoposition(chromosome, position)
+  start <- genoposition(loci$chromosome, loci$start)
+  stop <- genoposition(loci$chromosome, loci$stop)
+  gind <- findInterval(goff, start)
+  past <- findInterval(goff, stop + 1)
+  gind[gind != (1 + past)] <- NA
+  return(loci$locus[gind])
+}
+
+homozygous.loci <- function(genotype, locus) {
+  stopifnot(all.equal(levels(genotype), genotype.levels))
+  stopifnot(length(genotype) == length(locus))
+  fun <- function(gt) all(homozygous(gt))
+  df <- aggregate(genotype, list(locus=locus), fun)
+  is.homoz <- df$x
+  return(df$locus[is.homoz])
+}
+
